161ea8042b6da2a365c54240ec98f97fcf3bfacc
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-13 15:56:36 -0700

    working on episode

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,42 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+const testEpisode = {
+    id:1,
+    name: "",
+    image: "image",
+    season: 1,
+    number: 1,
+    summary: "This episode is about",
+    runtime: 1
+}
 
-test("renders the summary test passed as prop", ()=>{});
+const testImgEpisode = {
+    
+    id:2,
+    name: "",
+    image: null,
+    season: 1,
+    number: 2,
+    summary: "This episode is about",
+    runtime: 1
+}
 
-test("renders default image when image is not defined", ()=>{});
+test("renders without error", () => {
+    render(<Episode episode={testEpisode}/>)
+});
+
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>);
+
+    const summary = screen.queryAllByText(/This episode is about/i);
+
+    expect(summary).toHaveLength(1);
+
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={testImgEpisode}/>);
+    
+});
+
